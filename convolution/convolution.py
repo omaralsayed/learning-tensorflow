a8c17ecb19f96487b4fb24c4e3460bb98f01e625
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2019-12-06 18:39:38 -0500

    Fix spacing

--- convolution/convolution.py
+++ convolution/convolution.py
@@ -9,24 +9,31 @@
 
 # We don't have to do all the math for filtering and compressing,
 # we simply define convolutional and pooling layers to do the job for us.
+
 model = tf.keras.models.Sequential([
     # Ask Keras to generate 64 3x3 filters with a relu 
     # activation to throw the negative values away
     tf.keras.layers.Conv2D(64,(3,3),activation='relu',
+
     # Seek an input shape of 28x28 using a single byte for
     # color depth
         input_shape=(28,28,1)),
+
     # Create a maximum 2x2 pooling layer; so for every
     # four pixels, the biggest one will survive
     tf.keras.layers.MaxPooling2D(2,2),  
+
     # Add another convolutional layer,
     # and another max-pooling layer so that the network can
     # learn another set of convolutions on top of the existing 
     # one, and then again, pool to reduce the size.
     tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
+
     tf.keras.layers.MaxPooling2D(2,2),
+
     # Flatten the output shape from (None,5,5,64) to (None,1600)
     tf.keras.layers.Flatten(),
+    
     # Just the regular densely-connected NN layers
     tf.keras.layers.Dense(128,activation='relu'),
     tf.keras.layers.Dense(10,activation='software')
